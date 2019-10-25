f581c061a1dcdb61d24a9df2c300fda55a755c51
Peiyang Qiao <painterqiao@gmail.com>
2019-10-25 08:57:49 +0800

    feat(manager):waterfall,js + absolute

--- src/components/manage.vue
+++ src/components/manage.vue
@@ -15,13 +15,43 @@
             /*
             * learn: dom 节点的克隆和追加
             * cloneNode得到的是Node对象
+            *
+            * 这样实现瀑布流的缺点是，每列宽度是固定的，有可能导致很宽的横向间隔
             * */
-            // this.$refs["art"].display = "block";
+            let gap = 10; //垂直gap
+            let w = this.$refs["manager"].clientWidth;
+            let mod = Math.floor(w/200);
+            let g = (w - mod * 200)/(mod -1) //水平的间距
+
+            //初始化height
+            let height = [this.$refs["art"].offsetHeight];
+            for (let i = 1;i < mod;i ++){
+                height[i] = -10
+            }
+
             let els = [];
             for(let i = 0; i< 40; i++){
                 els[i] = this.$refs["art"].cloneNode(true);
                 els[i].firstChild.innerHTML = i + "\n文章".repeat(Math.floor(Math.random() * 100));
-                document.getElementById("manager").appendChild(els[i]);
+            }
+            let min = function () {
+                let r = 0;
+                for(let i=0, l = 99999999999;i<4;i++){
+                    if(height[i] < l){
+                        l = height[i]
+                        r = i
+                    }
+                }
+                return r
+            };
+            for(let e of els){
+                let m = min();
+                let w = m * (200.00000 + g);
+                let h = height[m] + gap;
+                e.style.top = h + "px";
+                e.style.left = w + "px";
+                document.getElementById("manager").appendChild(e);
+                height[m] += e.offsetHeight + gap;
             }
         }
     }
@@ -31,24 +61,22 @@
     #manager {
         max-width: 1024px;
         margin: 3em auto;
-        column-count:4;
-        column-gap: 2em;
+        position: relative;
+        height: 100%;
     }
 
     .art {
         border-radius: 4px;
-        width: 10em;
+        width: 200px;
         overflow-y: hidden;
-        margin: 1em 0.5em;
+        position: absolute;
         padding: 1em;
         background-color: #fff;
         box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
-        break-inside: avoid;
+        box-sizing: border-box;
     }
 
     .art:hover{
-        position: relative;
-        top: 5px;
         box-shadow: 2px 7px 4px rgba(0, 0, 0, 0.1);
     }
 
