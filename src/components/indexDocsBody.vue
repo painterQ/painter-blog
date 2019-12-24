dd1d37844ccfbf7da56d22f83d3a22b0c6cd6f96
Peiyang Qiao <painterqiao@gmail.com>
2019-12-24 15:32:38 +0800

    katelog

--- src/components/indexDocsBody.vue
+++ src/components/indexDocsBody.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <el-row class="index-body-all">
-            <el-col  :span="17" class="index-body-main">
+            <el-col :span="16" class="index-body-main">
                 <!-- learn: 插入HTML-->
-                <main v-html='document'></main>
+                <main id="doc-content" v-html="document"></main>
                 <div class="doc-bottom">
                     <h2>推荐文章</h2>
                     <ul>
@@ -19,10 +19,11 @@
                     </div>
                 </div>
             </el-col>
-            <el-col  :span="5" class="index-body-aside">
-                <strong class="tag-tital"></strong>
-                目录一<br>
-                目录二
+            <el-col :span="5" class="index-body-aside">
+<!--                <strong class="tag-tital"></strong>-->
+                <!-- 使用外部插件自动生成目录npm i katelog -S-->
+                <!-- https://github.com/KELEN/katelog-->
+                <div id="doc-cateLog"></div>
             </el-col>
         </el-row>
     </div>
@@ -31,12 +32,43 @@
 <script>
     import api from '../api/rpc'
     import message from "../api/message";
+    import kateLog from 'katelog';
     export default {
         name: "index-docs-body",
         data: function () {
-            return{
+            return {
+                kateLog: null,
                 document: "",
             }
+        },
+        watch:{
+            document(newV,oldV){
+                console.log("watch",newV,oldV)
+                this.kateLog.rebuild();
+            },
+        },
+        mounted() {
+            this.kateLog = new kateLog({
+                contentEl: 'doc-content',
+                catelogEl: 'doc-cateLog',
+                // linkClass: 'k-catelog-link',
+                // linkActiveClass: 'k-catelog-link-active',
+                // supplyTop: 20,
+                // selector: ['h2', 'h3'],
+                active: null
+            });
+            /*this.document = `<h1>标题</h1>
+                    <p>文章</p>
+                    <h2>1.二级标题</h2>
+                    <h3>1.1三级标题</h3>
+                    <p>文章</p>
+                    <h3>1.2三级标题</h3>
+                    <p>文章</p>
+                    <h2>2.二级标题</h2>
+                    <p>文章</p>
+                    <h2>3.二级标题</h2>
+                    <p>文章</p>`*/
+            this.render()
         },
         methods:{
             render(){
@@ -63,7 +95,7 @@
 </script>
 
 <style scoped>
-    .coffee{
+    .coffee {
         font-size: 28px;
         line-height: 58px;
         position: relative;
@@ -80,21 +112,26 @@
         background: #fccd60;
         background: linear-gradient(to bottom, #fccd60 0, #fbae12 100%, #2989d8 100%, #207cca 100%);
     }
-    .tag-tital{
+
+    .tag-tital {
 
     }
-    .tag-tital:before{
+
+    .tag-tital:before {
         content: "\f02e";
         margin-right: 0.5em;
     }
-    .index-body-main{
+
+    .index-body-main {
         margin: 2em;
     }
-    .index-body-aside{
-        border-left: rgba(88,88,88,0.1) 1px solid;
+
+    .index-body-aside {
+        border-left: rgba(88, 88, 88, 0.1) 1px solid;
         padding: 1em 1em 0 1em;
         margin-left: 1em;
     }
+
     /*p h1 h2 h3 h4 h5 h6*/
     main p{
 
